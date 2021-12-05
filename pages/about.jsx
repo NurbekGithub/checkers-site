022b8332bc36fabe811c01b39c4db61afd31ba4b
Nurbek <nurbek-2395@mail.ru>
2021-12-05 22:17:03 +0600

    add documents and about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,9 +1,17 @@
-import { Heading } from "@chakra-ui/layout";
+import { Box } from "@chakra-ui/layout";
+import { useEffect, useRef } from "react";
 
 export default function AboutPage() {
+  const $iframe = useRef(null);
+  function resize(e) {
+    e.target.style.height = `${e.target.contentWindow.document.body.scrollHeight}px`;
+  }
+  useEffect(() => {
+    $iframe.current.style.height = $iframe.current.contentWindow.document.body.scrollHeight + "px";
+  }, [])
   return (
-    <Heading>
-      О Нас
-    </Heading>
-  )
+    <Box d="flex">
+      <iframe width="100%" ref={$iframe} onLoad={resize} frameBorder="0" scrolling="no" src="/html/Дойбы ойыны туралы.html"/>
+    </Box>
+  );
 }
--- pages/documents.jsx
+++ pages/documents.jsx
@@ -10,6 +10,21 @@ export default function DocumentsPage() {
       <ListItem>
         <Link href="/Календарь 2022 год ШАШКИ.pdf" download="Календарь 2022 год ШАШКИ.pdf" color="teal.500">Календарь 2022 год ШАШКИ.pdf</Link>
       </ListItem>
+      <ListItem>
+        <Link href="/Official-Rules-of-the-game.pdf" download="Official-Rules-of-the-game.pdf" color="teal.500">Official-Rules-of-the-game.pdf</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/Rules-of-the-Rapid-and-Blitz-games.pdf" download="Rules-of-the-Rapid-and-Blitz-games.pdf" color="teal.500">Rules-of-the-Rapid-and-Blitz-games.pdf</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/Table_of_draw_745-03.07.2017-1.pdf" download="Table_of_draw_745-03.07.2017-1.pdf" color="teal.500">Table_of_draw_745-03.07.2017-1.pdf</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/Правила игры, регламент проведения соревнований.docx" download="Правила игры, регламент проведения соревнований.docx" color="teal.500">Правила игры, регламент проведения соревнований.docx</Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/Таблица-дебютов-жен-2016.pdf" download="Таблица-дебютов-жен-2016.pdf" color="teal.500">Таблица-дебютов-жен-2016.pdf</Link>
+      </ListItem>
     </List>
   </Box>
   )
