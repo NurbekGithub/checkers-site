2726e8abe9fb091e65985c86b069b66a330631f7
Nurbek <nurbek-2395@mail.ru>
2021-12-06 19:05:22 +0600

    zoom image

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { Layout } from '../components/Layout'
+import 'react-medium-image-zoom/dist/styles.css'
 
 function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -2,6 +2,7 @@ import { Image } from "@chakra-ui/image";
 import { Box, Heading } from "@chakra-ui/layout";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { BLOCKS } from "@contentful/rich-text-types";
+import Zoom from 'react-medium-image-zoom'
 import { client } from "../../contentClient";
 
 const richTextOptions = {
@@ -13,17 +14,19 @@ const richTextOptions = {
       switch (mimeGroup) {
         case "image":
           return (
-            <Image
-              float="left"
-              mb="12px"
-              mr="24px"
-              w="100%"
-              maxH="400px"
-              objectFit="cover"
-              title={title ? title : ""}
-              alt={description ? description : ""}
-              src={file.url}
-            />
+            <Zoom>
+              <Image
+                float="left"
+                mb="12px"
+                mr="24px"
+                w="100%"
+                maxH="400px"
+                objectFit="cover"
+                title={title ? title : ""}
+                alt={description ? description : ""}
+                src={file.url}
+              />
+            </Zoom>
           );
         default:
           return (
