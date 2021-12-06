66f76ed575d1b5ab15c8300ae1c89b8ea7be7721
Nurbek <nurbek-2395@mail.ru>
2021-12-06 18:57:30 +0600

    mail

--- pages/contacts.jsx
+++ pages/contacts.jsx
@@ -1,9 +1,11 @@
-import { Heading } from "@chakra-ui/layout";
+import { Box, Heading, Link, Text } from "@chakra-ui/layout";
 
 export default function ContactsPage() {
-  return (
-    <Heading>
+  return <Box>
+    <Heading mb={4}>
       Контакты
     </Heading>
-  )
+
+    <Text>email: <Link color="teal.500" href="mailto:Draughtskz@mail.ru">Draughtskz@mail.ru</Link></Text>
+  </Box>
 }
